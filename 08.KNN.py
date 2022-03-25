9ba57419f9104e31d86415392adce01e219c80ff
malfunctixn <malfunctixn@gmail.com>
2022-03-25 10:14:42 +0530

    KNN: Clean up
    Signed-off-by: malfunctixn <malfunctixn@gmail.com>

--- 08.KNN.py
+++ 08.KNN.py
@@ -1,14 +1,13 @@
 from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
-import numpy as np
 
-iris_dataset=load_iris()
-X_train, X_test, y_train, y_test = train_test_split(iris_dataset["data"], iris_dataset["target"])
+iris_datasets = load_iris()
+X_train,X_test,Y_train,Y_test = train_test_split(iris_datasets["data"],iris_datasets["target"])
 kn = KNeighborsClassifier()
-kn.fit(X_train, y_train)
+kn.fit(X_train,Y_train)
 prediction = kn.predict(X_test)
-print(f"ACCURACY: {kn.score(X_test, y_test)}")
-target_names = iris_dataset.target_names
-for pred,actual in zip(prediction,y_test):
-    print(f"Prediction is {target_names[pred]} Actual is {target_names[actual]}")
+print(f"Accuracy is {kn.score(X_test,Y_test)}\n\n")
+target_names = iris_datasets.target_names
+for pred,actual in zip(prediction,Y_test):
+    print(f"Prediction: {target_names[pred]}\n\nActual:{target_names[actual]}\n\n")
